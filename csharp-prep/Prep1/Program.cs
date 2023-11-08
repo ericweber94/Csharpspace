bfcf7030fd7f16baf7dabd8d601baf05ffe5860d
ericweber94 <ericweber94@gmail.com>
2023-11-07 21:28:07 -0700

    made name program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+    Console.Write("What is your first name? ");
+    string name_first = Console.ReadLine();
+
+    Console.Write("What is your last name? ");
+    string name_last = Console.ReadLine();
+
+    Console.WriteLine("");
+    
+    Console.WriteLine($"Your name is {name_last}, {name_first} {name_last}.");
     }
 }
+
+// What is your first name? Scott
+// What is your last name? Burton
+
+// Your name is Burton, Scott Burton.
