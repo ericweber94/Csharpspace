e1fa11ab18acae11e1cbec43f19c3cf18b024844
ericweber94 <ericweber94@gmail.com>
2023-11-07 22:00:55 -0700

    finished number guesser

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+   Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        
+        int guessCount = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+                guessCount = guessCount + 1;
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+                guessCount = guessCount + 1;
+            }
+            else
+            {
+                guessCount = guessCount + 1;
+                string textVersion = guessCount.ToString();
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {textVersion} guesses!");
+            }
+
+        }                    
     }
 }
