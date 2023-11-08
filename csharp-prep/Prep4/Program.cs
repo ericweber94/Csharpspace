bbb5a241c6e41c446b613ea3b9db5cadebb5f291
ericweber94 <ericweber94@gmail.com>
2023-11-07 22:10:11 -0700

    finished number list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+    List<int> numbers = new List<int>();
+        
+        int Number = -1;
+        while (Number != 0)
+        {
+            Console.Write("Enter a number (input 0 to end): ");
+            
+            string answer = Console.ReadLine();
+            Number = int.Parse(answer);
+            
+            if (Number != 0)
+            {
+                numbers.Add(Number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int numberA in numbers)
+        {
+            sum += numberA;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        
+        int max = numbers[0];
+
+        foreach (int numberB in numbers)
+        {
+            if (numberB > max)
+            {
+                max = numberB;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
