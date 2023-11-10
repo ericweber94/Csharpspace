211cb337a67fa117adf95013600317cc0e38f2d6
ericweber94 <ericweber94@gmail.com>
2023-11-09 23:17:58 -0700

    full program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._title = "";
+        job1._company = "";
+        job1._startYear = 2010;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._title = "";
+        job2._company = "";
+        job2._startYear = 2010;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
